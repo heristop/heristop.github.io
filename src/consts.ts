7707f662b7c10b067cf2b238aa17f97d27940560
Alexandre Mogère <heristop@yahoo.fr>
2024-04-05 23:40:39 +0200

    chore(css): update colors

--- src/consts.ts
+++ src/consts.ts
@@ -1,4 +1,4 @@
-export const SITE_TITLE = 'Zazen Code';
+export const SITE_TITLE = '{ Zazen Code }';
 export const SITE_DESCRIPTION = 'Sharing practical tips and the latest trends in JavaScript, TypeScript and PHP';
 export const SITE_URL = 'https://heristop.github.io';
 export const SITE_AUTHOR = 'Alexandre Mogère';
