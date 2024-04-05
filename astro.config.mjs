93d1e2b2df60eb3b16f400fa10c97dc70e34b66e
Alexandre Mogère <heristop@yahoo.fr>
2024-04-05 23:40:39 +0200

    [Css] Up colors

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,4 @@ import sitemap from "@astrojs/sitemap";
 export default defineConfig({
   site: "https://heristop.github.io",
   integrations: [mdx(), sitemap()],
-  routes: ["/:category/:year/:month/:day/:title.html"],
 });
