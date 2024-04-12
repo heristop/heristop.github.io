1d8e508ddeccd49444a30b5a9165bf478193868c
Alexandre Mogère <heristop@yahoo.fr>
2024-04-13 00:10:19 +0200

    feat(md): auto target blank

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,13 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 
 import sitemap from "@astrojs/sitemap";
+import { externalLink } from "./src/plugins/externalLink";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://heristop.github.io",
+  markdown: {
+    rehypePlugins: [[externalLink, { domain: "heristop.github.io" }]],
+  },
   integrations: [mdx(), sitemap()],
 });
