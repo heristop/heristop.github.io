6c4365f5f776d7b53ff144d5f1d5d9c700050747
Alexandre Mogère <heristop@yahoo.fr>
2024-05-07 00:34:43 +0200

    feat(site): add hidden section

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,18 +4,15 @@ import sitemap from "@astrojs/sitemap";
 import { externalLink } from "./src/plugins/externalLink";
 import tailwind from "@astrojs/tailwind";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://heristop.github.io",
   markdown: {
-    rehypePlugins: [
-      [
-        externalLink,
-        {
-          domain: "heristop.github.io",
-        },
-      ],
-    ],
+    rehypePlugins: [[externalLink, {
+      domain: "heristop.github.io"
+    }]]
   },
-  integrations: [mdx(), sitemap(), tailwind()],
+  integrations: [mdx(), sitemap(), tailwind(), react()]
 });
