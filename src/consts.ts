a9c28c4875350168d91fc82a651502e98ec53b4d
Alexandre Mogère <heristop@yahoo.fr>
2024-08-30 14:47:56 +0200

    chore(footer): update role

--- src/consts.ts
+++ src/consts.ts
@@ -3,5 +3,5 @@ export const SITE_DESCRIPTION =
   "Zazen Code: Sharing practical tips and the latest trends in JavaScript, TypeScript and PHP";
 export const SITE_URL = "https://heristop.github.io";
 export const SITE_AUTHOR = "Alexandre Mogère";
-export const AUTHOR_ROLE = "Chapter Lead / Dev JS & PHP";
+export const AUTHOR_ROLE = "Tech Lead / Dev JS & PHP";
 export const LINKFOLIO_URL = "https://heristop.vercel.app";
