c53412863b57afe824348aad95d915a76f36324a
Alexandre Mogère <heristop@yahoo.fr>
2025-10-06 20:58:19 +0200

    feat(ui): improve ui footer

--- src/consts.ts
+++ src/consts.ts
@@ -5,3 +5,9 @@ export const SITE_URL = "https://heristop.github.io";
 export const SITE_AUTHOR = "heristop (Alexandre Mogère)";
 export const AUTHOR_ROLE = "Chapter Lead / Dev JS & PHP";
 export const LINKFOLIO_URL = "https://heristop.vercel.app";
+
+// Social Media Links
+export const GITHUB_URL = "https://github.com/heristop";
+export const LINKEDIN_URL = "https://linkedin.com/in/alexandre-mogere";
+export const X_URL = "https://x.com/heristop";
+export const BLUESKY_URL = "https://bsky.app/profile/heristop.bsky.social";
