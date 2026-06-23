38a530bdf6e2ab82f0d0a1cb0a80676ddd5d067c
Alexandre Mogère <heristop@yahoo.fr>
2026-06-23 11:42:58 +0200

    build(config): update astro config for v7

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,5 @@
 import { defineConfig } from "astro/config";
+import { unified } from "@astrojs/markdown-remark";
 import mdx from "@astrojs/mdx";
 import remarkMath from "remark-math";
 import rehypeKatex from "rehype-katex";
@@ -12,39 +13,40 @@ import icon from "astro-icon";
 // https://astro.build/config
 export default defineConfig({
   integrations: [mdx(), sitemap(), react(), icon()],
+  compressHTML: true,
   markdown: {
     shikiConfig: {
       theme: "rose-pine",
       wrap: false,
     },
-    rehypePlugins: [
-      [
-        externalLink,
-        {
-          domain: "heristop.github.io",
-        },
-      ],
-      rehypeKatex,
-      rehypeSlug,
-      [
-        rehypeAutolinkHeadings,
-        {
-          behavior: "prepend",
-          properties: {
-            className: ["heading-anchor"],
-            ariaLabel: "Link to this section",
+    processor: unified({
+      remarkPlugins: [remarkMath],
+      rehypePlugins: [
+        [
+          externalLink,
+          {
+            domain: "heristop.github.io",
           },
-          content: {
-            type: "text",
-            value: "#",
+        ],
+        rehypeKatex,
+        rehypeSlug,
+        [
+          rehypeAutolinkHeadings,
+          {
+            behavior: "prepend",
+            properties: {
+              className: ["heading-anchor"],
+              ariaLabel: "Link to this section",
+            },
+            content: {
+              type: "text",
+              value: "#",
+            },
           },
-        },
+        ],
       ],
-    ],
-    remarkPlugins: [remarkMath],
+    }),
   },
 
   site: "https://heristop.github.io",
-
-  viewTransitions: true,
 });
